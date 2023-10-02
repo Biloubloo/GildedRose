a0facff06179b96557f432fa3c00969d1d025cd5
Biloubloo <122588450+Biloubloo@users.noreply.github.com>
2023-10-02 09:15:41 +0000

    commit2

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -1,62 +1,105 @@
 package com.gildedrose;
 
 class GildedRose {
-  Item[] items;
+    Item[] items;
 
-  public GildedRose(Item[] items) {
-    this.items = items;
-  }
-
-  public void updateQuality() {
-    for (int i = 0; i < items.length; i++) {
-      if (!items[i].name.equals("Aged Brie")
-          && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-        if (items[i].quality > 0) {
-          if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-            items[i].quality = items[i].quality - 1;
-          }
-        }
-      } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1;
-
-          if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (items[i].sellIn < 11) {
-              if (items[i].quality < 50) {
-                items[i].quality = items[i].quality + 1;
-              }
-            }
-
-            if (items[i].sellIn < 6) {
-              if (items[i].quality < 50) {
-                items[i].quality = items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-
-      if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-        items[i].sellIn = items[i].sellIn - 1;
-      }
-
-      if (items[i].sellIn < 0) {
-        if (!items[i].name.equals("Aged Brie")) {
-          if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (items[i].quality > 0) {
-              if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].quality = items[i].quality - 1;
-              }
-            }
-          } else {
-            items[i].quality = items[i].quality - items[i].quality;
-          }
-        } else {
-          if (items[i].quality < 50) {
-            items[i].quality = items[i].quality + 1;
-          }
-        }
-      }
+    public GildedRose(Item[] items) {
+        this.items = items;
     }
-  }
+
+    public void updateQuality() {
+        // Parcourt tous les éléments dans le tableau
+        for (Item item : items) {
+            // Vérifie le type de l'élément et effectue la mise à jour appropriée
+            if (isNormalItem(item)) {
+                // Pour les éléments normaux
+                updateNormalItem(item);
+            } else if (isAgedBrie(item)) {
+                // Pour "Aged Brie"
+                updateAgedBrie(item);
+            } else if (isSulfuras(item)) {
+                // Pour "Sulfuras", pas de mise à jour nécessaire
+            } else if (isBackstagePasses(item)) {
+                // Pour "Backstage Passes"
+                updateBackstagePasses(item);
+            }
+        }
+    }
+
+    private boolean isNormalItem(Item item) {
+        // Vérifie si l'élément est de type normal
+        return !isAgedBrie(item) && !isSulfuras(item) && !isBackstagePasses(item);
+    }
+
+    private boolean isAgedBrie(Item item) {
+        // Vérifie si l'élément est "Aged Brie"
+        return item.name.equals("Aged Brie");
+    }
+
+    private boolean isSulfuras(Item item) {
+        // Vérifie si l'élément est "Sulfuras"
+        return item.name.equals("Sulfuras, Hand of Ragnaros");
+    }
+
+    private boolean isBackstagePasses(Item item) {
+        // Vérifie si l'élément est "Backstage Passes"
+        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
+    }
+
+    private void updateNormalItem(Item item) {
+        // Mise à jour pour les éléments normaux
+        decreaseQuality(item);
+        decreaseSellIn(item);
+        if (item.sellIn < 0) {
+            // Si la date de péremption est passée, la qualité diminue deux fois plus vite
+            decreaseQuality(item);
+        }
+    }
+
+    private void updateAgedBrie(Item item) {
+        // Mise à jour pour "Aged Brie"
+        increaseQuality(item);
+        decreaseSellIn(item);
+        if (item.sellIn < 0) {
+            // Si la date de péremption est passée, la qualité augmente deux fois plus vite
+            increaseQuality(item);
+        }
+    }
+
+    private void updateBackstagePasses(Item item) {
+        // Mise à jour pour "Backstage Passes"
+        increaseQuality(item);
+        if (item.sellIn < 11) {
+            increaseQuality(item);
+        }
+        if (item.sellIn < 6) {
+            increaseQuality(item);
+        }
+        decreaseSellIn(item);
+        if (item.sellIn < 0) {
+            // Après la date de péremption, la qualité tombe à zéro
+            item.quality = 0;
+        }
+    }
+
+    private void increaseQuality(Item item) {
+        // Incrémente la qualité de l'élément si elle est inférieure à 50
+        if (item.quality < 50) {
+            item.quality++;
+        }
+    }
+
+    private void decreaseQuality(Item item) {
+        // Décrémente la qualité de l'élément si elle est supérieure à zéro
+        if (item.quality > 0) {
+            item.quality--;
+        }
+    }
+
+    private void decreaseSellIn(Item item) {
+        // Décrémente la date de péremption de l'élément (sauf pour "Sulfuras")
+        if (!isSulfuras(item)) {
+            item.sellIn--;
+        }
+    }
 }
