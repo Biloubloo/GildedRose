51b52ce803930533387ed6751f2d29e1ba6778f0
Biloubloo <122588450+Biloubloo@users.noreply.github.com>
2023-10-09 08:33:46 +0000

    commit 4

--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -30,59 +30,59 @@ class GildedRoseTest {
 @Test
 @DisplayName("Test La qualité se dégrade deux fois plus vite apres la date de péremption")
 void testQualityDegradesTwiceAsFastAfterSellIn() {
-    // Créez un produit avec une qualité initiale de 10 et une date de péremption de 0 jours
-    Item item = new Item("Sample Product", 0, 10);
+  // Créez un produit avec une qualité initiale de 10 et une date de péremption de 0 jours
+  Item item = new Item("Sample Product", 0, 10);
 
-    // Créez une instance de GildedRose avec le produit
-    GildedRose gildedRose = new GildedRose(new Item[]{item});
+  // Créez une instance de GildedRose avec le produit
+  GildedRose gildedRose = new GildedRose(new Item[]{item});
 
-    // Appelez la méthode updateQuality pour effectuer la première mise à jour
-    gildedRose.updateQuality();
-    int qualityAfterFirstUpdate = item.quality;
+  // Appelez la méthode updateQuality pour effectuer la première mise à jour
+  gildedRose.updateQuality();
+  int qualityAfterFirstUpdate = item.quality;
 
-    // Appelez à nouveau la méthode updateQuality pour effectuer la deuxième mise à jour
-    gildedRose.updateQuality();
+  // Appelez à nouveau la méthode updateQuality pour effectuer la deuxième mise à jour
+  gildedRose.updateQuality();
 
-    // Vérifiez que la qualité a diminué de 2 unités après la date de péremption
-    assertEquals(qualityAfterFirstUpdate - 2, item.quality, "Quality should degrade twice as fast after sellIn");
+  // Vérifiez que la qualité a diminué de 2 unités après la date de péremption
+  assertEquals(qualityAfterFirstUpdate - 2, item.quality, "Quality should degrade twice as fast after sellIn");
 }
 
 
   @Test
   @DisplayName ("Test La qualité ne dépasse pas 50")
   void testQualityNeverExceeds50() {
-    // Créez un produit avec une qualité initiale de 50 et une date de péremption de 5 jours
+  // Créez un produit avec une qualité initiale de 50 et une date de péremption de 5 jours
     Item item = new Item("dfhdhf", 5, 50);
 
-      // Créez une instance de GildedRose avec le produit
-      GildedRose gildedRose = new GildedRose(new Item[]{item});
+    // Créez une instance de GildedRose avec le produit
+    GildedRose gildedRose = new GildedRose(new Item[]{item});
 
-      // Appelez la méthode updateQuality plusieurs fois pour vérifier que la qualité ne dépasse pas 50
-      for (int i = 0; i < 10; i++) {
-          
-          assertTrue(item.quality <= 50, "Qualité ne dépasse pas 50");
-      }
+    // Appelez la méthode updateQuality plusieurs fois pour vérifier que la qualité ne dépasse pas 50
+    for (int i = 0; i < 10; i++) {
+        
+        assertTrue(item.quality <= 50, "Qualité ne dépasse pas 50");
+    }
     }
 
   @Test
   @DisplayName ("Sulfura ne périme pas") 
   void testSulfurasNeverExpiresOrLosesQuality() {
-      // Créez un produit Sulfuras avec une qualité initiale de 80 (maximum) et une date de péremption de 10 jours
-      Item sulfuras = new Item("Sulfuras, Hand of Ragnaros", 10, 80);
+    // Créez un produit Sulfuras avec une qualité initiale de 80 (maximum) et une date de péremption de 10 jours
+    Item sulfuras = new Item("Sulfuras, Hand of Ragnaros", 10, 80);
 
-      // Créez une instance de GildedRose avec le produit Sulfuras
-      GildedRose gildedRose = new GildedRose(new Item[]{sulfuras});
+    // Créez une instance de GildedRose avec le produit Sulfuras
+    GildedRose gildedRose = new GildedRose(new Item[]{sulfuras});
 
-      // Appelez la méthode updateQuality plusieurs fois pour vérifier que la qualité et la date de péremption ne changent pas
-      for (int i = 0; i < 10; i++) {
-          
-          assertEquals(80, sulfuras.quality, "Qualité reste à 80");
-          assertEquals(10, sulfuras.sellIn, "Date de péremption reste à 10");
-      }
+    // Appelez la méthode updateQuality plusieurs fois pour vérifier que la qualité et la date de péremption ne changent pas
+    for (int i = 0; i < 10; i++) {
+        
+        assertEquals(80, sulfuras.quality, "Qualité reste à 80");
+        assertEquals(10, sulfuras.sellIn, "Date de péremption reste à 10");
+    }
   }
-@Test
-@DisplayName("Test Backstage passes")
-void testBackstagePassesQuality() {
+  @Test
+  @DisplayName("Test Backstage passes")
+  void testBackstagePassesQuality() {
     // Créez un produit Backstage pass avec une qualité initiale de 10 et une date de péremption de 12 jours
     Item backstagePass = new Item("Backstage passes to a TAFKAL80ETC concert", 12, 10);
 
@@ -98,9 +98,9 @@ void testBackstagePassesQuality() {
 
   
 }
-@Test
-@DisplayName("Test behavior of Backstage passes")
-void testBackstagePassesQualityEntre5et10() {
+  @Test
+  @DisplayName("Test behavior of Backstage passes")
+  void testBackstagePassesQualityEntre5et10() {
     // Créez un produit Backstage pass avec une qualité initiale de 10 et une date de péremption de 12 jours
     Item backstagePass = new Item("Backstage passes to a TAFKAL80ETC concert", 6, 20);
 
@@ -116,9 +116,9 @@ void testBackstagePassesQualityEntre5et10() {
 
   
 }
-@Test
-@DisplayName("Test Backstage passes")
-void testBackstagePassesQualityEntre0et5() {
+  @Test
+  @DisplayName("Test Backstage passes")
+  void testBackstagePassesQualityEntre0et5() {
     // Créez un produit Backstage pass avec une qualité initiale de 10 et une date de péremption de 12 jours
     Item backstagePass = new Item("Backstage passes to a TAFKAL80ETC concert", 4, 20);
 
@@ -134,9 +134,9 @@ void testBackstagePassesQualityEntre0et5() {
 
   
 }
-@Test
-@DisplayName("Test Backstage passes")
-void testBackstagePassesQualityà0() {
+  @Test
+  @DisplayName("Test Backstage passes")
+  void testBackstagePassesQualityà0() {
     // Créez un produit Backstage pass avec une qualité initiale de 10 et une date de péremption de 12 jours
     Item backstagePass = new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20);
 
@@ -147,5 +147,20 @@ void testBackstagePassesQualityà0() {
     assertEquals(-1, backstagePass.sellIn, "SellIn should decrease by 1");
     assertEquals(0, backstagePass.quality, "Quality should drop to 0");
 } 
+  @Test
+  @DisplayName("Test Conjured item quality degradation")
+  void testConjuredItemQualityDegradation() {
+    // Créez un élément "Conjured" avec une qualité initiale de 10 et une date de péremption de 5 jours
+    Item conjuredItem = new Item("Conjured", 5, 10);
+
+    // Créez une instance de GildedRose avec l'élément "Conjured"
+    GildedRose gildedRose = new GildedRose(new Item[]{conjuredItem});
+
+    // Appelez la méthode updateQuality pour vérifier la dégradation de qualité accélérée
+    gildedRose.updateQuality();
+
+    // La qualité devrait avoir diminué de 2
+    assertEquals(8, conjuredItem.quality, "Conjured item quality should degrade twice as fast");
+}
 
 }
